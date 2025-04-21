238431f2de3eaaa94a9b4c649066a3469de446d7
Nutthapat Pongtanyavichai <contact@leomotors.me>
2025-04-22 00:56:16 +0700

    docker for local development

---
+++ minio_docker_prepare.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+docker exec yuri_s3 mc alias set docker http://localhost:9000 admin password
+docker exec yuri_s3 mc mb docker/yuri-chat
+# docker exec yuri_s3 mc anonymous set public docker/yuri-chat
